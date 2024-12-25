350f01356bbf1200997cc9860960c6c636527a06
Sabari <Sabari@LAPTOP-OCES5AU6>
2024-12-25 15:23:07 +0530

    SeleniumDemoProject2024

---
+++ Sabarib/src/main/java/SeleniumProjectSampleNew/Sabarib/Broswerlaunch.java
@@ -0,0 +1,36 @@
+package SeleniumProjectSampleNew.Sabarib;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+
+public class Broswerlaunch {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+		
+		driver.manage().window().maximize();
+		
+		Thread.sleep(5000);
+		
+		WebElement myTextBox = driver.findElement(By.id("my-text-id"));
+		myTextBox.sendKeys("Sabari");
+		
+		driver.findElement(By.name("my-password")).sendKeys("crmsfa");
+		
+		driver.findElement(By.name("my-textarea")).sendKeys("Beasant Technology");
+
+		Thread.sleep(5000);
+		
+		driver.close();	
+		
+
+	}
+
+}
